eb482122ef313a07ad26757d0dee78a30f09b845
mariaantoniacci <mariaantoniacci19@gmail.com>
2024-10-03 15:21:10 +0200

    Update my_first_script.R

--- Code/my_first_script.R
+++ Code/my_first_script.R
@@ -1,1 +1,19 @@
+#there are 3 concepts:
+#1. object
+#2. the assignemnt (<-)
+# comment (like this one!)
 
+2 + 3 #this is my first operation in R 
+
+#it's important to make use of objects
+cato <- 2 + 3 #the result of the operation is assigned to an object called 'cato'
+
+
+maria <- 4 + 2
+maria 
+
+cato + maria
+
+cato ^ maria
+
+(cato + maria) ^ cato 
