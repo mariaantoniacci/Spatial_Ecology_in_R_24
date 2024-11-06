27d2b904efac063eec28dd220bead8951145669e
Maria Antoniacci <mariaantoniacci19@gmail.com>
2024-11-06 15:35:39 +0100

    Create 04_communities_multivar.R

---
+++ Code/04_communities_multivar.R
@@ -0,0 +1,60 @@
+# community level
+
+# code for multivariate analysis of species x plot data in communities
+
+#install package from CRAN vegan 
+
+install.packages("vegan")
+library(vegan)
+
+data(dune)
+
+dune # i obtain the matrix 
+
+head(dune) # first 6 lines restricted for each sample 6 plots 
+
+View(dune) # to see back the all table remember capital letter
+
+# in the package vegan the analysis is fast and simple with decorana of the dataset dune detrended corresposndace analysis and basic reciprocal avaraging
+# useful when the range is quite spread out and wide, it compacts mathemathically the data redicimg dimensions and widespread of data
+
+# Analysis
+decorana(dune)
+multivar <- decorana(dune) 
+# 4 axis of lenghts( amount of range represented by the first axis) 
+# what the % of the original is represented by the component 1
+
+
+dca1 = 3.7004
+dca2 = 3.1166
+dca3 = 1.30055
+dca4 =  1.47888
+
+total = dca1 + dca2 + dca3 + dca4 
+
+# dca1 lenght divided by the total times 100 to get the %
+
+# Proportions 
+prop1 = dca1/total
+prop2 = dca2/total
+prop3 = dca3/total
+prop4 = dca4/total
+
+# Percetanges
+perc1 = prop1*100
+perc2 = prop2*100
+perc3 = prop3*100
+perc4 = prop4*100
+
+# from original table matrix to a 2 dimension graph
+
+# Whole amouont of variability in %
+perc1 + perc2
+
+# instead of using ex 20 plots i compact dataset to final set of two axis by losing 29% of the original information but it worth it 
+
+# the first two axis explain 71% of the variability 
+
+plot(multivar)
+
+# now i can see which species are mopre correlated 
