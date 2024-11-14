76e4828324f3c362e32750ff540be6bd6fd272b7
Maria Antoniacci <mariaantoniacci19@gmail.com>
2024-11-14 11:52:05 +0100

    Update 02_population_density.R

--- Code/02_population_density.R
+++ Code/02_population_density.R
@@ -1,12 +1,12 @@
 # how to calculate density of individuals in the population
-# to import a packages install.packages.function
+# to import a packages install.packages is the function to use
 # let's use the function
 install.packages("spatstat") # "" protects from exit R in and out of package
 # to use the package library () without quotes because it's already in R now
 # recalling the package
 library(spatstat)
 
-# use dataset bei in R it tells how many point in the databaseand soem info on window 
+# use dataset bei in R: it tells how many points in the database and some info on window 
 bei
 
 # to get a plot a visual perception of dataset
@@ -14,7 +14,7 @@ bei
 plot(bei)
 
 # let's change the graph w/ closed dots
-plot(bei, pch=19)# remember to put space between argumets but avoid between brakes and function
+plot(bei, pch=19) # remember to put space between argumets but avoid between brakes and function
 
 plot(bei, pch=19, cex=.5)
 
@@ -25,7 +25,7 @@ bei.extra # list of pixel images raster type
 
 plot(bei.extra)
 
-# let's look at elev elevation so we extract one of the two image or elements
+# let's look at elev (elevation) so we extract one of the two image which are elements
 # extracting data assigning an element to object elevation and linking elev to bei.extra using $ as a "rope" 
 elevation <- bei.extra$elev
 
@@ -35,20 +35,23 @@ plot(elevation)
 # i just want the first element from bei.extra using [[]] and a number 1 without using $
 
 elevation2 <- bei.extra[[1]]
+
 # or if i have just an object 
+
 plot(bei.extra[[1]]) # [[]] more powerful beacause i dont'have to remember the name of the element i need
 plot(elevation2)
 
 
-# density is the function inside spatstat to calculate densuty of population
-# density map starting from points
-density(bei) # put desnity in an obj
+# density is the function inside spatstat to calculate density of population
+# let's build a density map starting from points
+density(bei) 
+# put density in an obj
 densitymap <- density(bei)
-densitymap #so every pixels has points inside
+densitymap # so every pixel has points inside
 plot(densitymap)
 
-# is there a possibility to show togheter the map of density and the original points
-points(bei) # puts point on prevoius graph plot(densitymap)
+# Is there a possibility to show togheter the map of density and the original points?
+points(bei) # puts points on prevoius graph plot (densitymap)
 points(bei, col="green")
 
 ######DAY2
