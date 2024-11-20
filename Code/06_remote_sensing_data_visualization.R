524d872045107b6a0579a207231af7bb313a60fd
Maria Antoniacci <mariaantoniacci19@gmail.com>
2024-11-20 15:36:01 +0100

    Update 06_remote_sensing_data_visualization.R

--- Code/06_remote_sensing_data_visualization.R
+++ Code/06_remote_sensing_data_visualization.R
@@ -26,7 +26,7 @@ plot(b2,col=cl)
 
 #b3 is bend for 560nm so import the image
 
-# Exercise: import b3 and plot with the previous palette
+# Exercise: import b3 green and plot with the previous palette
 b3<- im.import("sentinel.dolomites.b3.tif")
 plot(b3, col=cl)
 
@@ -53,10 +53,44 @@ plot(b8, col=cl)
 sentstack<- c(b2, b3, b4, b8)
 plot(sentstack, col=cl)
 
+# i want to plot only one layer of the data 
+dev.off
+plot(sentstack[[1]], col=cl) 
+# or 
+plot(sentstack[[4]], col=cl)
+
+# multifrae with different col palette
+# note taht if you plot only a band as b2, range blue-yellow readable for daltonics as well
+par(mfrow=c(2,2))
+
+clb<- colorRampPalette(c("dark blue", "blue", "light blue")) (100)
+plot(b2, col=clb)
+
+# Exercise: plot the same for the  band b3, b4, b8
+clb<- colorRampPalette(c("dark green", "green", "light green")) (100)
+plot(b3, col=clb)
+
+clb<- colorRampPalette(c("dark red", "red", "pink")) (100)
+plot(b4, col=clb)
+
+clb<- colorRampPalette(c("brown", "orange", "yellow")) (100)
+plot(b8, col=clb)
 
 
+# b8 has the higher discriminated resolution, many use this band
+# bend and combine the colours together in RGB: red green and blue used by devices to get other colours
+# we assign to each band a layer of the stack RGB
 
+# RGB plotting with a function im.plotRGB to get a natural colour image
+# sentsatck [[1]] blue
+# sentsatck [[2]] green
+# sentsatck [[3]] red
+# sentsatck [[4]] NIR
 
+im.plotRGB(sentstack, r=3, g=2, b=1) 
 
+# RGB has 3 component but we have 4 bands
+# facciamo scorrere i numeri we get false colours image so to add additional information that our eyes cannot perceive
+im.plotRGB(sentstack, r=4, g=3, b=2) 
 
-
+# why do that? vegetation is now represented by red 
