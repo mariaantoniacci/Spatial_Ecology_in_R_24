8a9c1490d5940ddd570fe1b418d4d711a6ec1aa8
Maria Antoniacci <mariaantoniacci19@gmail.com>
2024-12-13 13:05:43 +0100

    Update 09_external_data.R

--- Code/09_external_data.R
+++ Code/09_external_data.R
@@ -2,7 +2,9 @@
 
 library(terra)
 
-# download image from Nasa Earth Observatory, save it 
+# download image from Nasa Earth Observatory, save it in your folder
+
+# set the working directory from which the image is taken 
 
 setwd("C://Users/anton/OneDrive/Documents/MAGISTRALE/SPATIAL ECOLOGY IN R")
 getwd()
@@ -12,10 +14,15 @@ getwd()
 scotland <- rast("scotland_outerhebrides_oli_20240918_lrg.jpg")
 plotRGB(scotland, r=1, g=2, b=3)
 
-# alternative for library terra
+# alternative function to plot for library terra
 plot(scotland)
 
 
-# Exercise: dowload a set of the Earth Observatoryand upload it in R
+# Exercise: dowload an image from Earth Observatory and upload it in R
 Patagonian_Shelf_Waters_Abloom <- rast("bloom_pace_20241130_lrg.jpg")
 plot(Patagonian_Shelf_Waters_Abloom)
+
+
+# for Windows users: 
+# working directory works with / not \
+# you should add manually the extension of the image (ex. .jpg)
