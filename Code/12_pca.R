5e5f4e2a401e2f5bf43d60dea36ed7d5a497501a
Maria Antoniacci <mariaantoniacci19@gmail.com>
2024-12-13 14:36:42 +0100

    Update 12_pca.R

--- Code/12_pca.R
+++ Code/12_pca.R
@@ -1,24 +1,28 @@
-# from a multisystem of band 1 and band 2 i obtain a single axis pc1 of the st dev 
+# Multivariate analysis
+
+# from a multisystem of band 1 and band 2 i obtain a pc1 of the st dev 
 
 library(terra)
 library(imageRy)
 im.list()
 sent <- im.import("sentinel.png")
 
-# whtas the correlation of all the bands
+# what is the correlation between all the bands?
 pairs(sent)
 
-# we can have everything together
+# we can have everything together calculating PCA
 sentpc <- im.pca(sent)
 
+# analysis gives back 4 values of st. dev for each band (4 in total)
 # we have variability for the pca components 
-# pc1 is the first principal component 
-# pc1 in general has the higher variability 82
+# sentpc has 3 principal component (one equals 0) 
+# we want pc1 beacause of its higher variability or st. dev. (77)
 
-# sentpc has 3 principal component buit we want pc1
+# we also see on the below matrix correlations between original bands (sentinel 1,2,3,4)
+# and the extracted components 
 
 pc1 <- sentpc[[1]]
 
+# calculate st. dev. ontop of PC1 
 pc1stdev <- focal(pc1, matrix(1/9, 3,3), fun=sd)
-
-# pc1 map to use variability 
+plot(pc1stdev)
