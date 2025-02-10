24df0364afcd52d20b0149076434c629be7907eb
Maria Antoniacci <mariaantoniacci19@gmail.com>
2025-02-10 16:53:00 +0100

    Update 01_my_first_script.R

--- Code/01_my_first_script.R
+++ Code/01_my_first_script.R
@@ -22,28 +22,28 @@ cato ^ maria
 
 
 # arrays or vectors
-andrea <- c(10, 15, 20, 50, 70) # concatenated every in a function structuredf with parenthesis
+andrea <- c(10, 15, 20, 50, 70) # concatenated objects in a function 
 # all arguments are separted by commas in a function
 andrea
 
 sofia <- c(100, 200, 300, 400, 500)
 
-# now i want to correlate andrea e sofia, reationship between amount of co2 and the fruits 
+# now i want to correlate andrea and sofia, eg. reationship between amount of co2 and the fruits 
 # i use function 'plot'
 
 plot(sofia, andrea)
 
-#i can chnage stuff in the plot
+# i can change stuff in the plot
 # symbols, as black filled circles
-# pCh R software: each number associated with a symbol 
-# the site is https://www.datanovia.com/en/blog/pch-in-r-best-tips/
+# pch R software: each number associated with a symbol 
+# check the site https://www.datanovia.com/en/blog/pch-in-r-best-tips/
 
 plot(sofia, andrea, pch=19) # sofia here is an argument but no space between parenthesis 
 
-plot(sofia, andrea, pch=19, cex=2) # to modify dimension of points bigger 
+plot(sofia, andrea, pch=19, cex=2) # cex to modify dimension of points bigger 
 
-plot(sofia, andrea, pch=19, cex=0.5) # to modify dimension of points 0.5 or .5 smaller
+plot(sofia, andrea, pch=19, cex=0.5) # to modify dimension of points 0.5 or smaller
 
-plot(sofia, andrea, pch=19, cex=2, col="blue") # to modify color in https://r-graph-gallery.com/42-colors-names.html
+plot(sofia, andrea, pch=19, cex=2, col="blue") # col to modify color in https://r-graph-gallery.com/42-colors-names.html
 
 plot(sofia, andrea, pch=19, cex=2, col="blue", xlab="C02", ylab="amount of fruits") # to modify labels 
