c6a7502b01809031291bf41856f11c33bc918659
Maria Antoniacci <mariaantoniacci19@gmail.com>
2025-02-11 16:09:34 +0100

    Update 11_variability.R

--- Code/11_variability.R
+++ Code/11_variability.R
@@ -26,6 +26,7 @@ im.plotRGB(sent, 2,3,1)
 # measure standard deviation on a chosen band
 
 # obj sent is made of 4 elements, let's choose only the first 
+# let's associate the band to an object
 nir <- sent[[1]]
 
 # calculate st. dev. with the function focal()
@@ -42,6 +43,8 @@ stdev3 <- focal(nir, matrix(1/9, 3,3), fun=sd)
 
 # exercise: matrix of 7x7 
 stdev7 <- focal(nir, matrix(1/49, 7,7), fun=sd)
+# the bigger the window the bigger the standard deviation
+# it's because we're taking a bigger area 
 
 # multiframe to show difference between the variabilities calculated with two differently built windows
 par(mfrow=c(1,2))
@@ -51,3 +54,10 @@ plot(stdev7)
 # alternative using stack to plot two images together
 stdevstack <- c(stdev3, stdev7) 
 plot(stdevstack)  
+
+# notes:
+# bigger windows mean lower resolution
+# how to choose dimension of the matrix?
+# there's no right or wrong, try with different windows and choose the best 
+
+
